807977e8c427302447292634c18ec32ba61b681a
Vadim4Official <pivovarov.03@outlook.com>
2022-04-16 17:03:45 +0500

    16 april

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,31 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/my-name', function () {
+    return 'Пивоваров Вадим Павлович';
+});
+
+Route::get('/my-friend', function () {
+   return 'список друзей епта';
+});
+
+Route::get('/get-friend/{name}', function ($name){
+    return $name;
+});
+
+Route::get('/my-city/{city}', function ($city){
+    return $city;
+});
+
+Route::get('/level/{lvl}', function ($lvl){
+    return 'бобр';
+});
+
+Route::get('/power', function (){
+    $url = route('power');
+    return $url;
+});
+
+Route::get('/test', function (){
+})->name('power');
